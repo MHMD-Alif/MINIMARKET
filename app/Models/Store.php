8b244f9c9a0548f736c68e5b60ae6bd5ae2e6883
SultanAang <sultanaang45@gmail.com>
2025-01-11 12:17:04 +0700

    Create Store.php

---
+++ app/Models/Store.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Store extends Model
+{
+    protected $table = 'stores';
+    public $timestamps = false;
+    use HasFactory, HasRoles;
+
+    protected $fillable = ['name','image_url', 'location'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
